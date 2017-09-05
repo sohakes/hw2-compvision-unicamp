ff7b8e05467e8fe01664cc7b8dfb7c1f528f1ebf
Rafael Tomazela <sohakes@gmail.com>
2017-09-05 18:32:06 -0300

    sunrise

---
+++ src/hw2.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import math
+
+################  HW2  #####################
+# Nathana Facion                 RA:191079
+# Rafael Mariottini Tomazela     RA:192803
+############################################
+
+
+def main():
+    print("sunrise")
+
+if __name__ == '__main__':
+   main()
