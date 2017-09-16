bb13169ae04480d28b8a903176654a39b7a80a07
Rafael Tomazela <sohakes@gmail.com>
2017-09-16 00:50:12 -0300

    Sift done, not sure if it works

--- src/Sift.py
+++ src/Sift.py
@@ -4,6 +4,10 @@ from utils import *
 import math
 import itertools
 
+#TODO: não tem a ver com o Sift, só pra lembrar: provavelmente colocar todos na frame do PRIMEIRO frame
+#pra não acumular erro. Explicar o porque
+
+#TODO: colocar algo pra falar que frame tá também, no geral
 class Sift:
     #I let it be constant at 0.5, I doubt there is a reason to change that    
     def _calc_delta(self, octave_level): 
@@ -168,33 +172,160 @@ class Sift:
     def _does_discard_keypoint(self, w, s, m, n, c_edge):
         return self._calc_edgeness(w,s,m,n) > ((c_edge +1)**2)/c_edge
 
-    def _get_contribution(self, dog, keypoint, lam):
-        kpval, s, m, n, doc_oct, rx, ry, gamma = keypoint
+    def _gradient_m_n(self, w, s, m, n):
+        return (0.5*(w[s][m+1][n] - w[s][m-1][n]), 0.5*(w[s][m][n+1] - w[s][m][n-1]))
 
-    #lam=1.5
-    def _calculate_histogram(self, dog, keypoint, lam):
-        _, s, _, _, doc_oct, x, y, gamma = keypoint
+    #lam=1.5, n_bins=36, threshold=0.8
+    def _calculate_reference(self, dog, keypoint, lam, n_bins, threshold):
+        _, s, _, _, dog_oct, x, y, gamma = keypoint
         delta = self._calc_delta(dog_oct)
-        w = dog[doc_oct]
+        w = dog[dog_oct]
         wid, h = w[s].shape
         rang = (3 * lam * gamma)
         minx = math.ceil((-rang+x)/delta)
         maxx = math.floor(1 + (rang+x)/delta)
         miny = math.ceil((-rang+y)/delta)
         maxy = math.floor(1 + (rang+y)/delta)
-        if minx < 0 or maxx > wid or miny < 0 or maxy > h:
+
+        if minx <= 0 or maxx >= wid or miny <= 0 or maxy >= h:
             return None
+        
+        bins = [0] * n_bins
 
-        
-        
-        #return w[s][minx:maxx, miny:maxy]
-        
+        #calculating the histogram
+        for m in range(minx, maxx):
+            for n in range(miny,maxy):
+                exponent = -(math.sqrt(math.pow(delta * m - x,2) + math.pow(delta * n - y,2)))/2*((lam*gamma)**2)
+                grad = self._gradient_m_n(w, s, m, n)
+                grad_norm = math.sqrt(grad[0]**2 + grad[1]**2)
+                contribution = math.exp(exponent) * grad_norm
 
-    def _gradient_m_n(self, w, s, m, n):
-        return (0.5*(w[s][m+1][n] - w[s][m-1][n]), 0.5*(w[s][m][n+1] - w[s][m][n-1]))
+                #TODO TALVEZ ESTEJA TROCADO, se der problema, ver, mas acho que é indiferente (se eu errar igual)
+                bin_index = int(round((n_bins/(2*math.pi))*np.arctan2(grad[0], grad[1])))
+
+                bins[bin_index] += contribution
+
+        #smoothing histogram with 6 times circular convolution
+        kernel = [1/3, 1/3, 1/3]
+        bins = np.array(bins)
+        #print(len(bins), "len bins f")
+        for i in range(6):
+            temp_bins = np.concatenate(([bins[-1]], bins, [bins[0]]))
+            bins = np.convolve(temp_bins, kernel, mode='valid')
+            #print(len(bins), "len bins")
+        
+        #calculate reference point
+        maxbin = max(bins)
+        keypoints = []
+        for k in range(len(bins)):
+            hk = bins[k]
+            hkm = bins[(k - 1) % n_bins]
+            hkp = bins[(k + 1) % n_bins]
+            if hk > hkm and hk > hkp and hk >= threshold*maxbin:
+                thetak = 2*math.pi*(k-1)/n_bins
+                theta = (thetak + (math.pi/n_bins)* ((hkm-hkp)/(hkm-2*hk+hkp)))
+                keypoints.append(keypoint + (theta,))
+
+        return keypoints
+
+    #lam_descr = 6, n_hist = 4, n_ori = 8
+    def _normalized_descriptor(self, dog, keypoint_theta, lam_descr, n_hist, n_ori):
+        kpval, s, mt, nt, dog_oct, x, y, gamma, theta = keypoint_theta
+        delta = self._calc_delta(dog_oct)
+        def calc_normalized_cord(m, n):
+            nx = ((m * delta - x) * math.cos(theta) + (n * delta - y) * math.sin(delta))/gamma
+            ny = (-(m * delta - x) * math.sin(theta) + (n * delta - y) * math.cos(delta))/gamma
+            return (nx, ny)
+
+        wid, hei = dog[1][0].shape
+        rang = math.sqrt(2) * lam_descr * gamma               
+
+        ration_hist = ((n_hist+1)/n_hist)
+        rang2 = rang * ration_hist
+        minx = int(round((x - rang2)/delta))
+        maxx = int(round((x + rang2)/delta)) + 1
+        miny = int(round((y - rang2)/delta))
+        maxy = int(round((y + rang2)/delta)) + 1
+
+        if minx < 0 or maxx >= wid or miny < 0 or maxy >= hei:
+            return None 
+
+        histograms = [[[0]*n_ori]*n_hist]*n_hist
+
+        for m in range(minx, maxx):
+            for n in range(miny, maxy):
+                nx, ny = calc_normalized_cord(m, n)
+                #Verify if the sample (m, n) is inside the normalized patc
+                if max(abs(nx), abs(ny)) >= lam_descr * ration_hist:
+                    continue
+                
+                #Compute normalized gradient orientation
+                grad = self._gradient_m_n(dog[dog_oct], s, m, n)
+                ntheta = (np.arctan2(grad[0], grad[1]) - theta) % 2*math.pi
+                print("ntheta", ntheta)
+
+                #compute contribution
+                exponent = -((math.pow(delta * m - x,2) + math.pow(delta * n - y,2)))/(2*((lam_descr*gamma)**2))
+                grad_norm = math.sqrt(grad[0]**2 + grad[1]**2)
+                contribution = math.exp(exponent) * grad_norm
+                #print("contrib", contribution, exponent, math.exp(exponent), grad_norm, delta, math.pow(delta * m - x,2), lam_descr, gamma)
+                for i in range(1, n_hist +1):
+                    for j in range(1, n_hist + 1):
+                        xi = (i - (1+n_hist)/2) * (2*lam_descr/n_hist)
+                        yj = (j - (1+n_hist)/2) * (2*lam_descr/n_hist)
+                        if abs(xi - nx) > (2*lam_descr/n_hist) or abs(yj - ny) > (2*lam_descr/n_hist):
+                            continue
+
+                        for k in range(1, n_ori + 1):
+                            thetak = 2*math.pi*(k-1)/n_ori
+                            if abs((thetak - ntheta)%2*math.pi) >= 2*math.pi/n_ori:
+                                continue
+                            histograms[i-1][j-1][k-1] = (histograms[i-1][j-1][k-1]
+                                + (1 - ((n_hist)/(2*lam_descr))*abs(xi-nx))
+                                * (1 - ((n_hist)/(2*lam_descr))*abs(yj-ny))
+                                * (1 - ((n_ori)/(2*math.pi))*abs((thetak - ntheta)%2*math.pi))
+                                * contribution) 
+
+                            #print("hist", i, j, k, histograms[i-1][j-1][k-1])
+        print(histograms)
+        feature_vector = [0]*(n_hist*n_hist*n_ori)
+        for i in range(n_hist):
+            for j in range(n_hist):
+                for k in range(n_ori):
+                    feature_vector[i*n_hist*n_ori+j*n_ori+k] = histograms[i][j][k]
+
+        feature_vector = np.array(feature_vector)
+        fnorm = np.linalg.norm(feature_vector)
+        norm_thresh = 0.2 * fnorm        
+        print("fnorm", fnorm)
+
+        feature_vector[feature_vector > norm_thresh] = norm_thresh
+
+        for le in range(n_hist*n_hist*n_ori):
+            feature_vector[le] = min(math.floor(512*feature_vector[le]/fnorm), 255)
+
+        return feature_vector
+                
+
+    def _describe_keypoints(self, dog, minimas, maximas, lam, n_bins, threshold, lam_descr, n_hist, n_ori):
+        keypoints = minimas + maximas
+        final_features = []
+        for kp in keypoints:
+            kpthetas = self._calculate_reference(dog, kp, lam, n_bins, threshold)
+            if kpthetas is None:
+                continue
+            for kpt in kpthetas:
+                if kpt is None:
+                    continue
+                fv = self._normalized_descriptor(dog, kpt, lam_descr, n_hist, n_ori)
+                final_features.append((kpt, fv))
+
+        print("FINAL FEATURES YAY",final_features)
+        return final_features
+                
             
 
-    def _get_minimas_maximas(self, dog_octaves, c_edge):
+    def _get_minimas_maximas(self, dog_octaves, c_edge, gaussian):
         maximas = []
         minimas = []
         #print('important!', len(dog_octaves), len(dog_octaves[0]))
@@ -245,7 +376,7 @@ class Sift:
                         continue  
                     
                     #print(current_img[x,y], x, y, "CURR IMG XY")
-                    subpixel = self._get_subpixel_keypoint(current_dog_octave, s, x, y, doc_oct, gaussian, total_octaves)
+                    subpixel = self._get_subpixel_keypoint(current_dog_octave, s, x, y, doc_oct, gaussian, len(dog_octaves))
                     if subpixel is None:
                         #print('no subpixel')
                         continue
@@ -274,24 +405,26 @@ class Sift:
         #print("the mighty", kk)
         return (minimas, maximas)
 
-    def _describe_keypoints(self, dog_octaves, c_edge, minimas, maximas):
+    
 
                     
     #IMPORTANT: the c_edge value is for [0,1], I think it's still ok since it's a ratio
-    def get_descriptors(self, img_color, n_octaves=4, n_levels_octave=5, kernel_size=7, gaussian=0.8, c_edge=10):
+    def get_descriptors(self, img_color, n_octaves=4, n_levels_octave=5, kernel_size=7,
+            gaussian=0.8, c_edge=10, lam=1.5, n_bins=36, threshold=0.8, lam_descr = 6, n_hist = 4, n_ori = 8):
         #img must be black and white
         img_u = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY).astype(float)/255      
         w, h = img_u.shape 
         img = cv2.resize(img_u, dsize=(h*2, w*2), interpolation= cv2.INTER_LINEAR)        
         g_octaves = self._get_gaussian_pyramid(img, n_octaves, n_levels_octave, kernel_size, gaussian)
         dog_octaves = self._get_dog_pyramid(g_octaves)
-        (minimas, maximas) = self._get_minimas_maximas(dog_octaves, c_edge)
-        #TODO: step to improve by removing low contrast areas, but maybe whatever this
-        print('minmax',minimas + maximas)
+        (minimas, maximas) = self._get_minimas_maximas(dog_octaves, c_edge, gaussian)
+    
+        kpt_fv = self._describe_keypoints(dog_octaves, minimas, maximas, lam, n_bins, threshold, lam_descr, n_hist, n_ori)
+
         img_temp = img_color
         print(img_temp.shape)
-        for kpval, s, m, n, octv, rx, ry, gamma in minimas + maximas:
-            print((m*(2**(octv-1)), n*(2**(octv-1))))
+        for kpt, fv in kpt_fv:
+            kpval, s, mt, nt, doc_oct, rx, ry, gamma, theta = kpt
             img_temp = cv2.circle(img_temp,(int(round(ry)), int(round(rx))), 2, (0,255,0), 2)
 
         debug('img', img_temp)
